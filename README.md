c46d69acca3cb5b3ca4d3db4dadfb57722b743ec
Chris Seieroe <kaelad02@users.noreply.github.com>
2024-02-29 12:06:31 -0800

    add blind player checks

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# kaelad-custom-5e
-Foundry VTT module to customize the dnd5e system
+# Kaelad's Kustomizations
+
+This Foundry VTT module adds some customizations to the dnd5e system.
+
+## Automatically Mark Defeated
+
+Automatically apply the Bleeding condition when a GM-owned token is at or below half their hit points. Apply the Dead condition when they reach zero hit points and Unconscious if they're a PC.
+
+## Bug Fix for Players Identifying Items
+
+There's currently a bug in the dnd5e system that allows players to identify items on their character sheet. This hides the Identify toggle on the sheet and cancels any attempt to update the item from a macro for players.
+
+## Blind Player Checks
+
+There is a hidden setting that forces any ability check, save, skill, and tool roll by players to be blind rolls. Useful if you want players to make a roll but not know the outcome. It's not exposed in the settings, but a macro can toggle it on/off.
+
+```js
+const blind = !game.settings.get("kaelad-custom-5e", "blindPlayerChecks");
+await game.settings.set("kaelad-custom-5e", "blindPlayerChecks", blind);
+if (blind) ui.notifications.warn("Player checks are now blind");
+else ui.notifications.info("Player checks are now normal");
+```
--- src/kaelad-custom.js
+++ src/kaelad-custom.js
@@ -4,6 +4,8 @@ Hooks.once("init", () => {
   Hooks.on("updateActor", wounded);
 
   preventPlayerIdentifying();
+
+  blindPlayerChecks();
 });
 
 /**
@@ -73,6 +75,33 @@ function preventPlayerIdentifying() {
     if (game.user.isGM || item.system.identified) return;
     buttons.findSplice((e) => e.name === "DND5E.Identify");
   });
+}
+
+/**
+ * Blind Player Checks
+ */
+
+function blindPlayerChecks() {
+  // register setting and get current value
+  game.settings.register("kaelad-custom-5e", "blindPlayerChecks", {
+    name: "Blind Player Checks",
+    hint: "Force ability checks, saves, skills, and tool rolls by players to be blind rolls.",
+    scope: "world",
+    config: false,
+    type: Boolean,
+    default: false,
+    onchange: (value) => (_blindPlayerChecks = value),
+  });
+  let _blindPlayerChecks = game.settings.get("kaelad-custom-5e", "blindPlayerChecks");
+
+  // register hooks for Ability, Skill, and Tool checks
+  const hookFn = (_, config) => {
+    if (!game.user.isGM && _blindPlayerChecks) config.rollMode = CONST.DICE_ROLL_MODES.BLIND;
+  };
+  Hooks.on("dnd5e.preRollAbilityTest", hookFn);
+  Hooks.on("dnd5e.preRollAbilitySave", hookFn);
+  Hooks.on("dnd5e.preRollSkill", hookFn);
+  Hooks.on("dnd5e.preRollToolCheck", hookFn);
 }
 
 /**
