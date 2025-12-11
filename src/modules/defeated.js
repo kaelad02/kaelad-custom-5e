d4ddba7be20b7fd40ede205519d5e637f92f2428
Chris Seieroe <kaelad02@users.noreply.github.com>
2025-12-11 03:18:37 -0800

    rewrite defeated with new hooks and v13 functions

--- src/modules/defeated.js
+++ src/modules/defeated.js
@@ -1,47 +1,31 @@
 /**
- * Wounded indicators
+ * Mark tokens as defeated
  */
 
 export function init() {
-  Hooks.on("updateActor", wounded);
+  Hooks.on("dnd5e.damageActor", (actor, changes, update, userId) => {
+    if (userId !== game.user.id) return;
+    if (actor.system.attributes.hp.value > 0) return;
+    if (knockout(actor)) {
+      actor.toggleStatusEffect("unconscious", {active: true, overlay: false});
+    } else {
+      actor.toggleStatusEffect("dead", {active: true, overlay: true});
+    }
+  });
+
+  Hooks.on("dnd5e.healActor", (actor, changes, update, userId) => {
+    if (userId !== game.user.id) return;
+    if (actor.system.attributes.hp.value === 0) return;
+    if (knockout(actor)) {
+      actor.toggleStatusEffect("unconscious", {active: false});
+    } else {
+      actor.toggleStatusEffect("dead", {active: false});
+    }
+  });
 }
 
-async function wounded(actor, update, options, userId) {
-  // only run hook on the user that updated the actor
-  if (userId !== game.user.id) return;
-
-  // only run hook if HP was changed (any of them)
-  if (getProperty(update, "system.attributes.hp") === undefined) return;
-
-  debug("actor HP updated, checking for wounded/dead states");
-  const hp = actor.system.attributes.hp;
-
-  // Bloodied
-  if (!actor.hasPlayerOwner) {
-    const isBloodied = 0 < hp.value && hp.value <= (hp.max + hp.tempmax) / 2;
-    await toggleEffect(actor, "bleeding", isBloodied);
-  }
-
-  if (actor.type === "character") {
-    // Unconscious
-    const isUnconscious = hp.value === 0;
-    await toggleEffect(actor, "unconscious", isUnconscious);
-  } else {
-    // Dead
-    const isDead = hp.value === 0;
-    await toggleEffect(actor, "dead", isDead, true);
-  }
-}
-
-async function toggleEffect(actor, effectId, active, overlay = false) {
-  debug("toggling effect", effectId, active);
-
-  // adapted from EffectsElement#_onToggleCondition
-  const existing = actor.effects.get(dnd5e.utils.staticID(`dnd5e${effectId}`));
-  if (existing && !active) return existing.delete();
-  else if (!existing && active) {
-    const effect = await ActiveEffect.implementation.fromStatusEffect(effectId);
-    if (overlay) effect.updateSource({"flags.core.overlay": true});
-    return ActiveEffect.implementation.create(effect, {parent: actor, keepId: true});
-  }
+function knockout(actor) {
+  let knockout = actor.type === 'character';
+  knockout ||= actor.type === "npc" && (!foundry.utils.isEmpty(actor.classes) || actor.system.traits.important);
+  return knockout;
 }
