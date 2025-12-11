b4a874c981d22f0dfd55266ce2b7c36c2a07aa6e
Chris Seieroe <kaelad02@users.noreply.github.com>
2025-12-11 03:35:54 -0800

    check for setting in hook instead of saving a local copy

--- src/modules/blind-player-checks.js
+++ src/modules/blind-player-checks.js
@@ -3,7 +3,6 @@
  */
 
 export function init() {
-  // register setting and get current value
   game.settings.register("kaelad-custom-5e", "blindPlayerChecks", {
     name: "Blind Player Checks",
     hint: "Force ability checks, saves, skills, and tool rolls by players to be blind rolls.",
@@ -11,13 +10,11 @@ export function init() {
     config: false,
     type: Boolean,
     default: false,
-    onChange: (value) => (_blindPlayerChecks = value),
   });
-  let _blindPlayerChecks = game.settings.get("kaelad-custom-5e", "blindPlayerChecks");
 
-  // register hooks for Ability, Skill, and Tool checks
   const hookFn = (config, dialog, message) => {
-    if (!game.user.isGM && _blindPlayerChecks) message.rollMode = CONST.DICE_ROLL_MODES.BLIND;
+    if (!game.user.isGM && game.settings.get("kaelad-custom-5e", "blindPlayerChecks"))
+      message.rollMode = CONST.DICE_ROLL_MODES.BLIND;
   };
   Hooks.on("dnd5e.preRollAbilityCheckV2", hookFn);
   Hooks.on("dnd5e.preRollAbilitySaveV2", hookFn);
