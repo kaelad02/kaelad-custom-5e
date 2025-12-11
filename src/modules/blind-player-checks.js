85917d4601f7d36535bb3a9e9bcc9a6cf01556fa
Chris Seieroe <kaelad02@users.noreply.github.com>
2025-12-11 03:33:45 -0800

    rewrite BPC with new hooks

--- src/modules/blind-player-checks.js
+++ src/modules/blind-player-checks.js
@@ -11,16 +11,14 @@ export function init() {
     config: false,
     type: Boolean,
     default: false,
-    onchange: (value) => (_blindPlayerChecks = value),
+    onChange: (value) => (_blindPlayerChecks = value),
   });
   let _blindPlayerChecks = game.settings.get("kaelad-custom-5e", "blindPlayerChecks");
 
   // register hooks for Ability, Skill, and Tool checks
-  const hookFn = (_, config) => {
-    if (!game.user.isGM && _blindPlayerChecks) config.rollMode = CONST.DICE_ROLL_MODES.BLIND;
+  const hookFn = (config, dialog, message) => {
+    if (!game.user.isGM && _blindPlayerChecks) message.rollMode = CONST.DICE_ROLL_MODES.BLIND;
   };
-  Hooks.on("dnd5e.preRollAbilityTest", hookFn);
-  Hooks.on("dnd5e.preRollAbilitySave", hookFn);
-  Hooks.on("dnd5e.preRollSkill", hookFn);
-  Hooks.on("dnd5e.preRollToolCheck", hookFn);
+  Hooks.on("dnd5e.preRollAbilityCheckV2", hookFn);
+  Hooks.on("dnd5e.preRollAbilitySaveV2", hookFn);
 }
