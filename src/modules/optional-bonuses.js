7884a9ff4e2f5e164159ab5c119bb94f4f7ec6b7
Chris Seieroe <kaelad02@users.noreply.github.com>
2025-12-17 14:32:11 -0800

    add two automatic bonuses, showing a checked and disabled checkbox

--- src/modules/optional-bonuses.js
+++ src/modules/optional-bonuses.js
@@ -8,10 +8,41 @@ export function init() {
   // TODO remove when system version updated to 5.2.0
   dnd5e.documents.ChatMessage5e.prototype._simplifyDamageRoll = newSimplifyDamageRoll;
 
+  // look for automatic bonuses earlier, before the buildDamageRollConfig hook
+  Hooks.on("dnd5e.preRollDamageV2", (config, dialog, message) => {
+    const item = config.subject.item;
+    const actor = config.subject.actor;
+
+    const getIdentifier = (identifier) => actor.items.find(i => i.system.identifier === identifier);
+    config.autoBonuses = {};
+
+    // Elemental Adept: Fire (by name since it doesn't actually force you to choose which element)
+    if (actor.items.getName("Elemental Adept: Fire")) {
+      // spells that do fire damage
+      if (item.type === "spell" && config.rolls[0].options.type === "fire") {
+        config.autoBonuses.elementalAdeptFire = {label: "Elemental Adept: Fire"};
+      }
+    }
+
+    // Great Weapon Fighting
+    const greatWeaponFighting = getIdentifier("great-weapon-fighting");
+    if (greatWeaponFighting) {
+      // melee weapon with Two-Handed or Versatile property, attacking w/ two hands
+      if (item.type === "weapon" &&
+        CONFIG.DND5E.weaponTypeMap[item.system.type.value] === "melee" &&
+        ["two", "ver"].some(p => item.system.properties.has(p)) &&
+        config.attackMode === "twoHanded") {
+        config.autoBonuses.greatWeaponFighting = {label: greatWeaponFighting.name};
+      }
+    }
+  });
+
   Hooks.on("dnd5e.buildDamageRollConfig", (dialog, rollConfig, formData, rollIndex) => {
     if (rollIndex > 0) return;
 
     const opts = formData?.object ?? {};
+    if (dialog.config.autoBonuses.elementalAdeptFire) addDieModifier(rollConfig, "min2");
+    if (dialog.config.autoBonuses.greatWeaponFighting) addDieModifier(rollConfig, "min3");
     if (opts.sneakAttack) rollConfig.parts.push("@scale.rogue.sneak-attack");
     if (opts.dreadfulStrikes) rollConfig.parts.push("@scale.fey.dreadful-strike[psychic]");
     if (opts.savageAttacker) rollConfig.parts[0] = `{${rollConfig.parts[0]}, ${rollConfig.parts[0]}}kh`;
@@ -28,6 +59,22 @@ export function init() {
 
     const getIdentifier = (identifier) => actor.items.find(i => i.system.identifier === identifier);
     const fields = [];
+
+    // Elemental Adept: Fire
+    const elementalAdeptFire = app.config.autoBonuses.elementalAdeptFire;
+    if (elementalAdeptFire) {
+      fields.push(
+        new foundry.data.fields.BooleanField({label: elementalAdeptFire.label}, {name: "elementalAdeptFire"})
+      );
+    }
+
+    // Great Weapon Fighting
+    const greatWeaponFighting = app.config.autoBonuses.greatWeaponFighting;
+    if (greatWeaponFighting) {
+      fields.push(
+        new foundry.data.fields.BooleanField({label: greatWeaponFighting.label}, {name: "greatWeaponFighting"})
+      );
+    }
 
     // Sneak Attack
     const sneakAttack = getIdentifier("sneak-attack");
@@ -89,7 +136,21 @@ export function init() {
   });
 }
 
+function addDieModifier(rollConfig, modifier) {
+  rollConfig.parts = rollConfig.parts.map(part => {
+    const roll = new Roll(part);
+    roll.terms.forEach(term => {
+      if (term instanceof foundry.dice.terms.DiceTerm) term.modifiers.push(modifier);
+    });
+    roll.resetFormula();
+    return roll.formula;
+  });
+}
+
 function addFieldset(fields, elements) {
+  // fields that are automatic, not optional
+  const auto = ["elementalAdeptFire", "greatWeaponFighting"];
+
   if (fields.length) {
     // make new fieldset to hold these
     const newFieldset = document.createElement("fieldset");
@@ -98,7 +159,12 @@ function addFieldset(fields, elements) {
     legend.innerText = "Optional Bonuses";
     newFieldset.append(legend);
     // add fields
-    fields.forEach(field => newFieldset.append(field.toFormGroup()));
+    fields.forEach(field => {
+      if (auto.includes(field.name))
+        newFieldset.append(field.toFormGroup({}, {disabled: true, value: true}));
+      else
+        newFieldset.append(field.toFormGroup());
+    });
     // add the new fieldset right before the existing fieldset
     const configFieldset = elements.querySelector('fieldset[data-application-part="configuration"]');
     configFieldset.before(newFieldset);
@@ -190,25 +256,25 @@ function funNewConfigureDamage({critical = {}} = {}) {
  * Copied from version 5.2.0 of the system
  */
 function newSimplifyDamageRoll(roll) {
-  const { OperatorTerm, NumericTerm, DiceTerm, PoolTerm } = foundry.dice.terms;
+  const {OperatorTerm, NumericTerm, DiceTerm, PoolTerm} = foundry.dice.terms;
   const termResultClasses = ["success", "failure", "rerolled", "exploded", "discarded"];
   const aggregate = {
     type: roll.options.type, total: Math.max(0, roll.total), constant: 0, dice: [], icon: null, method: null
   };
   let hasMultiplication = false;
-  for ( let i = roll.terms.length - 1; i >= 0; ) {
+  for (let i = roll.terms.length - 1; i >= 0;) {
     const term = roll.terms[i--];
-    if ( !(term instanceof NumericTerm) && !(term instanceof DiceTerm) && !(term instanceof PoolTerm) ) {
+    if (!(term instanceof NumericTerm) && !(term instanceof DiceTerm) && !(term instanceof PoolTerm)) {
       continue;
     }
     const value = term.total;
-    if ( term instanceof DiceTerm ) {
+    if (term instanceof DiceTerm) {
       const tooltipData = term.getTooltipData();
       aggregate.dice.push(...tooltipData.rolls);
       aggregate.icon ??= tooltipData.icon;
       aggregate.method ??= tooltipData.method;
     }
-    if ( term instanceof PoolTerm ) {
+    if (term instanceof PoolTerm) {
       term.rolls.forEach((poolTermRoll, i) => {
         // Get simplified data for each roll
         const simplified = this._simplifyDamageRoll(poolTermRoll);
@@ -216,7 +282,7 @@ function newSimplifyDamageRoll(roll) {
         // Apply main result classes to individual dice
         simplified.dice.forEach(die => {
           const resultClasses = termResultClasses.filter(c => result[c]).join(" ");
-          if ( resultClasses.length ) die.classes += ` ${resultClasses}`;
+          if (resultClasses.length) die.classes += ` ${resultClasses}`;
         });
         aggregate.dice.push(...simplified.dice);
         aggregate.icon ??= simplified.icon;
@@ -225,13 +291,13 @@ function newSimplifyDamageRoll(roll) {
     }
     let multiplier = 1;
     let operator = roll.terms[i];
-    while ( operator instanceof OperatorTerm ) {
-      if ( !["+", "-"].includes(operator.operator) ) hasMultiplication = true;
-      if ( operator.operator === "-" ) multiplier *= -1;
+    while (operator instanceof OperatorTerm) {
+      if (!["+", "-"].includes(operator.operator)) hasMultiplication = true;
+      if (operator.operator === "-") multiplier *= -1;
       operator = roll.terms[--i];
     }
-    if ( term instanceof NumericTerm ) aggregate.constant += value * multiplier;
+    if (term instanceof NumericTerm) aggregate.constant += value * multiplier;
   }
-  if ( hasMultiplication ) aggregate.constant = null;
+  if (hasMultiplication) aggregate.constant = null;
   return aggregate;
 }
