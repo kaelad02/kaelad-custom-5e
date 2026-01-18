c518c621ab5f8b9a7bb4920eddd312c176117869
Chris Seieroe <kaelad02@users.noreply.github.com>
2026-01-18 11:50:38 -0800

    add workaround for gray dice

--- src/modules/damage-type-appearance.js
+++ src/modules/damage-type-appearance.js
@@ -7,7 +7,9 @@ export function init() {
   const origEvaluate = dnd5e.dice.DamageRoll.prototype.evaluate;
   dnd5e.dice.DamageRoll.prototype.evaluate = async function(options={}) {
     const setting = game.settings.get("kaelad-custom-5e", "damageTypeAppearance");
-    if ( setting && this.options.type && !this.options.appearance?.colorset ) {
+    if ( setting && this.options.type && !this.options.appearance?.colorset &&
+      // workaround for https://gitlab.com/riccisi/foundryvtt-dice-so-nice/-/issues/536
+      game.dice3d?.exports?.COLORSETS?.[this.options.type] ) {
       foundry.utils.setProperty(this.options, "appearance.colorset", this.options.type);
     }
     return origEvaluate.bind(this).apply(options);
